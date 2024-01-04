7f719c1c270e8db2b8ea646abc3e12e957db816b
Darya <daryasamsonowicz@gmail.com>
2024-01-04 14:37:53 +0300

    refactor: add resources folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { UserModule } from './user/user.module';
-import { TrackModule } from './track/track.module';
-import { ArtistModule } from './artist/artist.module';
-import { AlbumModule } from './album/album.module';
-import { FavsModule } from './favs/favs.module';
+import { UserModule } from './resources/user/user.module';
+import { TrackModule } from './resources/track/track.module';
+import { ArtistModule } from './resources/artist/artist.module';
+import { AlbumModule } from './resources/album/album.module';
+import { FavsModule } from './resources/favs/favs.module';
 
 @Module({
   imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
